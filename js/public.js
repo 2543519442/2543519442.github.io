179180360d9f06a3f11fa9c8f8eaf85afb59efee
rain-blog <2543519442@qq.com>
2021-12-29 11:26:03 +0800

    My blog

--- js/public.js
+++ js/public.js
@@ -4,12 +4,12 @@ $(function(){
         console.log('页面加载完成')
         $('.loader').fadeOut(400)
         console.log('加载图隐藏')
-        $('#div-body').css({
-            'overflow-y':'auto',
-        })
-        console.log('滚动条出现')
-        $('.head-name').slideDown(600)
-        console.log('头部出现')
+        // $('#div-body').css({
+        //     'overflow-y':'auto',
+        // })
+        // console.log('滚动条出现')
+        // $('.head-name').slideDown(600)
+        // console.log('头部出现')
     })
     console.log('2')
 
